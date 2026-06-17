de06c401bd527f7bbdf9031364898e2079a7ec02
GuScarpim <gustavoscarpim@gmail.com>
2026-06-17 20:28:39 -0300

    update 3

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <h1>Muito legal legal legal</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
